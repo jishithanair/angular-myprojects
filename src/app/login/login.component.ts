9f874600ff4c0d98423b5818aa7f22844fcfe536
jishithanair <jishithanair15@gmail.com>
2021-07-10 22:38:19 +0530

    login form ui

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) {}
+  loginForm: FormGroup;
+  submitted: boolean = false;
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, validatePassword]]
+    });
   }
 
+  submit() {
+    this.submitted = true;
+  }
+}
+
+function validatePassword(c: FormControl) {
+  let exp = /^[a-zA-Z][0-1]$/;
+
+  return exp.test(c.value)
+    ? null
+    : {
+        passwordError: {
+          message: 'Password is invalid'
+        }
+      };
 }
